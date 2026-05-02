bf8eb9b4690f176e9781f4f3a9831847fc21578a
sunnymishraatf-lgtm <sunnymishra.atf@gmail.com>
2026-05-03 03:26:18 +0530

    final free groq api

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ async function getCodeFromAI(question) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            model: "llama3-8b-8192",
+            model: "llama-3.1-8b-instant",
             messages: [
                 {
                     role: "user",
